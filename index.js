5a619d5005007cf13d84515a613dd9551bfe7e89
Diego Moreno <dmore123@fiu.edu>
2023-09-30 01:06:59 -0400

    build

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.static('dist'))
-
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
